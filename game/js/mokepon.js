20b3c2f09ed658998bc1762d7a9905b9331c945f
Luis Tapia <devlou@protonmail.com>
2022-08-16 21:13:36 -0500

    Add event listener to pet selection button

---
+++ game/js/mokepon.js
@@ -0,0 +1,5 @@
+function selectPlayerPet() {
+  alert("YOU SELECTED YOUR PET!");
+}
+const btnPlayerPet = document.getElementById("pet-btn");
+btnPlayerPet.addEventListener("click", selectPlayerPet);
