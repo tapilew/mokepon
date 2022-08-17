2420eb17169fc7be00e6c72f40c3ba3b49432630
Luis Tapia <devlou@protonmail.com>
2022-08-16 21:49:23 -0500

    Alert name of selected pet

--- game/js/mokepon.js
+++ game/js/mokepon.js
@@ -1,5 +1,29 @@
-function selectPlayerPet() {
-  alert("YOU SELECTED YOUR PET!");
+function startGame() {
+  const btnPlayerPet = document.getElementById("pet-btn");
+  btnPlayerPet.addEventListener("click", selectPlayerPet);
 }
-const btnPlayerPet = document.getElementById("pet-btn");
-btnPlayerPet.addEventListener("click", selectPlayerPet);
+
+function selectPlayerPet() {
+  const radHipodoge = document.getElementById("hipodoge");
+  const radCapipepo = document.getElementById("capipepo");
+  const radRatigueya = document.getElementById("ratigueya");
+  let selectedPet = "";
+  let petEmoji = "";
+  if (radHipodoge.checked) {
+    selectedPet = "Hipodoge";
+    petEmoji = "🔥";
+  } else if (radCapipepo.checked) {
+    selectedPet = "Capipepo";
+    petEmoji = "💧";
+  } else if (radRatigueya.checked) {
+    selectedPet = "Ratigüeya";
+    petEmoji = "🌱";
+  }
+  let petSelectionMsg =
+    selectedPet !== "" && petEmoji !== ""
+      ? `You selected ${selectedPet.toUpperCase()}! ${petEmoji}`
+      : "No pet selected...";
+  alert(petSelectionMsg);
+}
+
+window.addEventListener("load", startGame);
